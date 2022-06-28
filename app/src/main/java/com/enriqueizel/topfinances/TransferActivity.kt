c64af1f44f3ce7b57b29d71a12a47e5b442f62b0
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-28 18:42:54 -0400

    feat: create navigation to TransferActivity

--- app/src/main/java/com/enriqueizel/topfinances/TransferActivity.kt
+++ app/src/main/java/com/enriqueizel/topfinances/TransferActivity.kt
@@ -2,10 +2,14 @@ package com.enriqueizel.topfinances
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enriqueizel.topfinances.databinding.ActivityTransferBinding
 
 class TransferActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityTransferBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_transfer)
+    binding = ActivityTransferBinding.inflate(layoutInflater)
+    setContentView(binding.root)
   }
 }
