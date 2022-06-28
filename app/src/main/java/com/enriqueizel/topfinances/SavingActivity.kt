e8ff738a4132d7f20adc2b8de09e85f9a0f1cdbf
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-28 19:01:29 -0400

    feat: add viewBinding in activity

--- app/src/main/java/com/enriqueizel/topfinances/SavingActivity.kt
+++ app/src/main/java/com/enriqueizel/topfinances/SavingActivity.kt
@@ -2,10 +2,14 @@ package com.enriqueizel.topfinances
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enriqueizel.topfinances.databinding.ActivitySavingBinding
 
 class SavingActivity : AppCompatActivity() {
+  private lateinit var binding: ActivitySavingBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_saving)
+    binding = ActivitySavingBinding.inflate(layoutInflater)
+    setContentView(binding.root)
   }
 }
