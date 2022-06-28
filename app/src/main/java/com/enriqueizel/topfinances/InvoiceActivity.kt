5770910b5630ada1d012e1d52da74339678cce40
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-28 17:31:14 -0400

    feat: create the toolbar and navigation to go back to MainActivity

--- app/src/main/java/com/enriqueizel/topfinances/InvoiceActivity.kt
+++ app/src/main/java/com/enriqueizel/topfinances/InvoiceActivity.kt
@@ -1,11 +1,28 @@
 package com.enriqueizel.topfinances
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enriqueizel.topfinances.databinding.ActivityInvoiceBinding
 
 class InvoiceActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityInvoiceBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_invoice)
+    binding = ActivityInvoiceBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    supportActionBar!!.hide()
+
+    val toolbar = binding.toolbarInvoice
+
+    toolbar.title = "Faturas"
+    toolbar.setBackgroundColor(getColor(R.color.light_blue))
+    toolbar.setTitleTextColor(getColor(R.color.white))
+    toolbar.setNavigationIcon(getDrawable(R.drawable.ic_arrow_back_ios_34))
+    toolbar.setTitleMargin(300, 0, 0, 0)
+    toolbar.setOnClickListener {
+      finish()
+    }
   }
 }
