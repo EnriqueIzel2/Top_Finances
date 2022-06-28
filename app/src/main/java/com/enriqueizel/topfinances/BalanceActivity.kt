0f3896e0e90fc4782c4b370de5b070e76aa7c260
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-28 15:21:35 -0400

    feat: add binding and title to toolbar

--- app/src/main/java/com/enriqueizel/topfinances/BalanceActivity.kt
+++ app/src/main/java/com/enriqueizel/topfinances/BalanceActivity.kt
@@ -2,12 +2,23 @@ package com.enriqueizel.topfinances
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enriqueizel.topfinances.databinding.ActivityBalanceBinding
 
 class BalanceActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityBalanceBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_balance)
-
+    binding = ActivityBalanceBinding.inflate(layoutInflater)
+    setContentView(binding.root)
     supportActionBar!!.hide()
+
+    val toolbar = binding.toolbarBalance
+
+    toolbar.title = "Saldo"
+    toolbar.setBackgroundColor(getColor(R.color.light_blue))
+    toolbar.setTitleTextColor(getColor(R.color.white))
+    toolbar.setTitleMargin(400, 0, 400, 0)
+
   }
 }
